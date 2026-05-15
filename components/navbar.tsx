388addd3f599da51b8ba1a05b49d554fa19a182d
nahuel <nahuelg976@gmail.com>
2026-05-15 16:30:13 -0300

    Fix navbar link clickable area

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 
@@ -8,24 +8,29 @@ import { MotionTransition } from "./transition-component";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-    const router = usePathname()
+  const router = usePathname();
 
-    return (
-        <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
-            <nav>
-                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
-                    {itemsNavbar.map((item) => (
-                        <div
-                            key={item.id}
-                            className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
-                            data-tooltip-target="tooltip-default">
-                            <Link href={item.link}>{item.icon} </Link>
-                        </div>
-                    ))}
-                </div>
-            </nav>
-        </MotionTransition>
-    );
-}
+  return (
+    <MotionTransition
+      position="right"
+      className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-10"
+    >
+      <nav>
+        <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
+          {itemsNavbar.map((item) => (
+            <Link
+              key={item.id}
+              className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && "bg-secondary"}`}
+              data-tooltip-target="tooltip-default"
+              href={item.link}
+            >
+              {item.icon}{" "}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </MotionTransition>
+  );
+};
 
 export default Navbar;
